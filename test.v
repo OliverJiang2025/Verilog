bf3771276a4d1235e5d692576075bec8b8c3a7cb
OliverJiang2025 <sitpole@outlook.com>
2025-09-28 11:40:50 +0100

    try

---
+++ test.v
@@ -0,0 +1,14 @@
+module counter(
+    input wire clk,
+    input wire rst_n,
+    output reg [3:0] q
+)
+
+always@(posedge clk or negedge rst_n) begin
+    if (!rst_n)
+        q <= 4'b0000;
+    else 
+        q <= q + 1'
+end
+
+endmodule
